d92f26c3e8616465d2440d3bfe156ccc63b17ac1
Alexander Jacobs <alex@ipindex.co>
2016-10-22 16:20:34 -0400

    no message

--- api/listings.js
+++ api/listings.js
@@ -2,9 +2,6 @@ var Controller = require('../controllers');
 
 module.exports = function(app) {
 
-    app.get('/listings/:listing_id',
-    	Controller.listings.getById
-    );
     app.get('/listings',
     	Controller.listings.getAll
     );
