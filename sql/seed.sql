b46451661cee51e7668fb7c1d96344055c86867b
Alexander Jacobs <alex@ipindex.co>
2016-10-29 20:55:12 -0400

    no message

--- sql/seed.sql
+++ sql/seed.sql
@@ -1382,7 +1382,7 @@ INSERT INTO Locations(title,minerals,address,state,latitude,longitude) VALUES ('
 INSERT INTO Locations(title,minerals,address,state,latitude,longitude) VALUES ('Sheffield Mine','ruby, sapphire','385 Sheffield Farms Rd, Franklin, NC 28734','North Carolina',35.27233,-83.393465);
 INSERT INTO Locations(title,minerals,address,state,latitude,longitude) VALUES ('Nethers Farm - Flint Ridge','flint','3680 Flint Ridge Rd. Hopewell, OH 43746','Ohio',40.000492,-82.184958);
 INSERT INTO Locations(title,minerals,address,state,latitude,longitude) VALUES ('Great Salt Plains National Wildlife Refuge','selenite','71189 Harper Road, Jet, OK 73749','Oklahoma',36.666142,-98.181179);
-INSERT INTO Locations(title,minerals,address,state,latitude,longitude) VALUES ('Double Eagle Mine','Oregon Sunstone','n/a','Oregon',42.42.758,-119.49.248);
+INSERT INTO Locations(title,minerals,address,state,latitude,longitude) VALUES ('Double Eagle Mine','Oregon Sunstone','n/a','Oregon',42.42758,-119.49248);
 INSERT INTO Locations(title,minerals,address,state,latitude,longitude) VALUES ('Richardson''s Rock Ranch','Agate, Thundereggs','6683 Hay Creek Rd, Madras, OR 97741','Oregon',44.732367,-120.979045);
 INSERT INTO Locations(title,minerals,address,state,latitude,longitude) VALUES ('Spectrum Sunstone Mines','Oregon Sunstone','n/a','Oregon',42 43.941,-119 52.007);
 INSERT INTO Locations(title,minerals,address,state,latitude,longitude) VALUES ('Big Thunder Gold Mine','Gold','604 Blair St, Keystone, SD 57751','South Dakota',43.895034,-103.417123);
